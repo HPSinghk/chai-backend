4d335dd06e8623bff24682f7679e8a394121d17a
Harendra Pratap Singh <hk284088@gmail.com>
2024-08-12 18:39:14 +0530

    env modified

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+        //upload file on cloudinary
+         const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinay",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved as the upload operaton got failed
+        return null; 
+    }
+}
+
+
+
+export { uploadOnCloudinary }
+
+
